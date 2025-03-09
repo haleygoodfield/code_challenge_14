a749f80e61a5011cfd17b7f12127e899f970c115
Haley Goodfield <haleygoodfield@usf.edu>
2025-03-09 12:32:32 -0400

    Task 3 - Highlighting High Priority Tickets

--- cc_14.js
+++ cc_14.js
@@ -16,6 +16,11 @@ function addSupportTicket(customerName, issueDesc, priorityLvl) {
     const priorityLabel = document.createElement("span"); // label indicating priority level.
     priorityLabel.textContent = priorityLvl; 
     priorityLabel.classList.add("priority-label");
+
+    // Task 3 
+    if (priorityLvl.trim().tooLowerCase === "high") { // High priority issues added to special class for highlighting 
+        priorityLabel.classList.add("high-priority");
+    }
     
     const resolveBtn = document.createElement("button"); // "Resolve" button to remove the ticket
     resolveBtn.textContent = "Resolve";
@@ -33,11 +38,26 @@ function addSupportTicket(customerName, issueDesc, priorityLvl) {
 
     // Appends the ticket to the container using appendChild
     container.appendChild(ticket);
+
+    // Highlights the ticket after adding a ticket
+    highlightHighPriorityTickets();
 }
 
-// Test Cases
-addSupportTicket("Bethany Mejia", "Printer not working", "Low Priority ");
-addSupportTicket("Deana Jefferson", "Cybersecurity breach", "High Priority ");
-addSupportTicket("Zara Holloway","Software problems", "Medium Priority ");
 
 // Task 3: Highlighting High Priority Tickets
+function highlightHighPriorityTickets() { // Use document.querySelectorAll to select all tickets with a "High" priority class
+    const highPriorityTickets = document.querySelectorAll(".high-priority"); 
+ 
+    //  Convert the NodeList into an array using Array.from() 
+    Array.from(highPriorityTickets).forEach(ticket => { // // Use an array method (forEach()) to update the appearance of high-priority tickets
+        ticket.parentElement.style.backgroundColor = "rgb(211, 174, 201)"; // Changing background color
+        ticket.parentElement.style.fontWeight = "bold"; // Change text to bold    
+    });
+}
+
+// Task 3 doesnt do anything?? 
+
+// Test Cases
+addSupportTicket("Bethany Mejia", "Printer not working", "Low");
+addSupportTicket("Deana Jefferson", "Cybersecurity breach", "High");
+addSupportTicket("Zara Holloway","Software problems", "Medium");
